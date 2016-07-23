d619f2d18f991c44cc8e4ecff65ccdbe989f7319
Shappiro <iemma.ron@gmail.com>
2016-07-23 22:00:33 +0200

    Packaged first integration of R in an example. Added some consideration on README

---
+++ PROJECTS/oms3.proj.RIntegration/src/example1/TryRenjin.java
@@ -0,0 +1,23 @@
+package example1;
+
+import javax.script.*;
+// ... add additional imports here ...
+
+public class TryRenjin {
+    public static void main(String[] args) throws Exception {
+        // create a script engine manager:
+        ScriptEngineManager manager = new ScriptEngineManager();
+        // create a Renjin engine:
+        ScriptEngine renjin = manager.getEngineByName("Renjin");
+        // check if the engine has loaded correctly:
+        if(renjin == null) {
+            throw new RuntimeException("Renjin Script Engine not found on the classpath.");
+        }
+        // R code here
+        renjin.eval("df <- data.frame(x=1:10, y=(1:10)+rnorm(n=10))");
+        renjin.eval("print(df)");
+        renjin.eval("print(lm(y ~ x, df))");
+        // ...Or, alternatively:
+        // engine.eval(new java.io.FileReader("script.R"));  }
+    }
+}
